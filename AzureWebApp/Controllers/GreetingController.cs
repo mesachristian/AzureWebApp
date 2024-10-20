86c26eebc2b9a63b6f2040526cdf36d9dee70270
Christian Mesa <mesachristian_coop@outlook.com>
2024-10-20 18:22:09 -0500

    Greeting controller added

---
+++ AzureWebApp/Controllers/GreetingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreetingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Greeting()
+        {
+            return Ok("Hi my name is Christian :3");
+        }
+    }
+}
